0f61ad3bbbea95afec08f630be6f04dfaf5dbbfd
Orpheus Johansson <123548945+DarkMantrid@users.noreply.github.com>
2023-12-08 13:42:56 +0100

    Update main.cpp

--- project2/src/main.cpp
+++ project2/src/main.cpp
@@ -1,14 +1,17 @@
 #include "../inc/node.hpp"
 #include "../inc/gpio_utils.hpp"
 
-
+/**********************************************************************
+ * @brief Main function controlling the machine learning system
+ * @return Exit status of the program
+ **********************************************************************/
 int main() {
-    yrgo::machine_learning::Node neuralNetwork;
+    yrgo::machine_learning::Node neuralNetwork; // Create an instance of the Node class
 
     // Initialize GPIO pins for buttons and LED
     neuralNetwork.InitializeGPIO();
 
-    // Run the system
+    // Run the system continuously
     while (true) {
         // Predict LED state based on button inputs and control LED
         neuralNetwork.PredictAndControlLED();
@@ -17,5 +20,5 @@ int main() {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
-    return 0;
+    return 0; // Return 0 to indicate successful execution
 }
