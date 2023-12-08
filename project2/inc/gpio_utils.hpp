c11955d27eb1fc7ac85996879e9530b09386aad3
Orpheus Johansson <123548945+DarkMantrid@users.noreply.github.com>
2023-12-08 13:43:25 +0100

    Update gpio_utils.hpp

--- project2/inc/gpio_utils.hpp
+++ project2/inc/gpio_utils.hpp
@@ -6,20 +6,56 @@
 
 namespace GPIOUtils {
 
+/**********************************************************************
+ * @brief Enum representing GPIO line direction
+ **********************************************************************/
 enum GPIODLineDirection { GPIOD_LINE_DIRECTION_IN, GPIOD_LINE_DIRECTION_OUT };
 
+/**********************************************************************
+ * @brief Enum representing GPIO line edge
+ **********************************************************************/
 enum GPIODLineEdge { GPIOD_LINE_EDGE_RISING, GPIOD_LINE_EDGE_FALLING, GPIOD_LINE_EDGE_BOTH };
 
+/**********************************************************************
+ * @brief Gets the GPIO chip
+ * @return Pointer to the GPIO chip
+ **********************************************************************/
 gpiod_chip* get_gpiod_chip0();
 
+/**********************************************************************
+ * @brief Creates a new GPIO line
+ * @param pin GPIO pin number
+ * @param direction Direction of the GPIO line
+ * @return Pointer to the created GPIO line
+ **********************************************************************/
 gpiod_line* gpiod_line_new(const uint8_t pin, const GPIODLineDirection direction);
 
+/**********************************************************************
+ * @brief Toggles the state of a GPIO line
+ * @param self Pointer to the GPIO line
+ **********************************************************************/
 void gpiod_line_toggle(gpiod_line* self);
 
+/**********************************************************************
+ * @brief Blinks a GPIO line at a specified speed
+ * @param self Pointer to the GPIO line
+ * @param blink_speed_ms Blink speed in milliseconds
+ **********************************************************************/
 void gpiod_line_blink(gpiod_line* self, const uint16_t blink_speed_ms);
 
+/**********************************************************************
+ * @brief Detects an event on a GPIO line based on edge detection
+ * @param self Pointer to the GPIO line
+ * @param edge Edge to detect (rising, falling, or both)
+ * @param previous_input Pointer to previous input state
+ * @return True if an event is detected, false otherwise
+ **********************************************************************/
 bool gpiod_line_event_detected(gpiod_line* self, const GPIODLineEdge edge, uint8_t* previous_input);
 
+/**********************************************************************
+ * @brief Delays execution for a specified time in milliseconds
+ * @param delay_time_ms Time to delay in milliseconds
+ **********************************************************************/
 void delay_ms(const uint16_t delay_time_ms);
 
 } // namespace GPIOUtils
