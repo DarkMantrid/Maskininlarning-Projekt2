d108a883e0d01adfb92b8e7cb8765d2f273e5bf8
Orpheus Johansson <123548945+DarkMantrid@users.noreply.github.com>
2023-12-08 13:18:19 +0100

    Update node.hpp

--- project2/inc/node.hpp
+++ project2/inc/node.hpp
@@ -1,29 +1,91 @@
 #pragma once
 
 #include <vector>
-#include <gpiod.h> // Include libgpiod headers
+#include <gpiod.h>
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <chrono>
+#include <thread>
 
 namespace yrgo {
 namespace machine_learning {
 
 class Node {
 public:
-    // Existing methods for Node class
+    Node();
+    ~Node();
 
-    // New method to handle prediction based on button inputs and LED control
+    void InitializeGPIO();
     void PredictAndControlLED();
 
-    // Methods for GPIO initialization, button reading, and LED control
-    void InitializeGPIO(); // Initialize GPIO pins
-    std::vector<int> ReadButtonStates(); // Read button states
-    void ControlLED(bool state); // Control LED based on prediction
-
 private:
-    // Add GPIO related members (GPIO chip, line pointers, etc.) here
     struct gpiod_chip *chip;
     struct gpiod_line *ledLine;
-    std::vector<struct gpiod_line*> buttonLines;
+    std::vector<struct gpiod_line *> buttonLines;
+
+    // Neural network parameters
+    int input_nodes;
+    int hidden_nodes;
+    int output_nodes;
+    std::vector<std::vector<double>> weights_input_hidden;
+    std::vector<std::vector<double>> weights_hidden_output;
+    std::vector<double> bias_hidden;
+    std::vector<double> bias_output;
+
+    std::vector<double> hidden_outputs; // Store outputs of hidden layer neurons
+    double learning_rate; // Learning rate for weight updates
+    std::vector<int> input; // Input data used in backpropagation
+    std::vector<double> output; // Actual output from the network during forward pass
+
+    // Neural network methods
+    std::vector<int> ReadButtonStates();
+    void ControlLED(bool state);
+    void TrainNetwork(const std::vector<std::vector<int>> &inputs, const std::vector<int> &targets, int epochs, double learning_rate);
+    double ReLU(double x);
+    void InitializeWeights();
+    void ForwardPropagation(const std::vector<int> &input);
+    void BackPropagation(int target);
+    int Predict(const std::vector<int> &input);
+
+    // Additional members for neural network functionality
+    void SetOutput(const std::vector<double> &new_output) { output = new_output; }
+    void SetHiddenOutputs(const std::vector<double> &new_hidden_outputs) { hidden_outputs = new_hidden_outputs; }
 };
+
+// Implement the methods inline or define them in a corresponding .cpp file
+// This might depend on the complexity and size of the methods
+
+inline Node::Node() : input_nodes(4), hidden_nodes(4), output_nodes(1) {
+    InitializeWeights();
+    InitializeGPIO();
+    output.resize(output_nodes);
+    hidden_outputs.resize(hidden_nodes);
+}
+
+inline Node::~Node() {
+    // Release GPIO resources
+    if (chip) {
+        gpiod_chip_close(chip);
+        chip = nullptr;
+    }
+    if (ledLine) {
+        gpiod_line_release(ledLine);
+        ledLine = nullptr;
+    }
+    for (auto line : buttonLines) {
+        if (line) {
+            gpiod_line_release(line);
+        }
+    }
+    buttonLines.clear();
+
+    // Release neural network resources if needed
+    // Add additional logic here for neural network resource cleanup if any
+}
+
+// Implement the methods inline or define them in a corresponding .cpp file
+// This might depend on the complexity and size of the methods
 
 } // namespace machine_learning
 } // namespace yrgo
