3812acc2f082a4cfad878d2ba6bc5b630f30b5df
Orpheus Johansson <123548945+DarkMantrid@users.noreply.github.com>
2023-12-08 13:43:38 +0100

    Update node.hpp

--- project2/inc/node.hpp
+++ project2/inc/node.hpp
@@ -11,41 +11,102 @@
 namespace yrgo {
 namespace machine_learning {
 
+/**********************************************************************
+ * @brief Class representing a Node in a machine learning system
+ **********************************************************************/
 class Node {
 public:
+    /**********************************************************************
+     * @brief Default constructor for Node class
+     **********************************************************************/
     Node();
+
+    /**********************************************************************
+     * @brief Destructor for Node class
+     **********************************************************************/
     ~Node();
 
+    /**********************************************************************
+     * @brief Initializes the GPIO chip and lines
+     **********************************************************************/
     void InitializeGPIO();
+
+    /**********************************************************************
+     * @brief Predicts and controls the LED based on the neural network's 
+     *        output
+     **********************************************************************/
     void PredictAndControlLED();
 
 private:
-    struct gpiod_chip *chip;
-    struct gpiod_line *ledLine;
-    std::vector<struct gpiod_line *> buttonLines;
+    struct gpiod_chip *chip; /**< Pointer to the GPIO chip */
+    struct gpiod_line *ledLine; /**< Pointer to the LED line */
+    std::vector<struct gpiod_line *> buttonLines; /**< Vector of pointers to button lines */
 
     // Neural network parameters
-    int input_nodes;
-    int hidden_nodes;
-    int output_nodes;
-    std::vector<std::vector<double>> weights_input_hidden;
-    std::vector<std::vector<double>> weights_hidden_output;
-    std::vector<double> bias_hidden;
-    std::vector<double> bias_output;
-
-    std::vector<double> hidden_outputs; // Store outputs of hidden layer neurons
-    double learning_rate; // Learning rate for weight updates
-    std::vector<int> input; // Input data used in backpropagation
-    std::vector<double> output; // Actual output from the network during forward pass
+    int input_nodes; /**< Number of input nodes */
+    int hidden_nodes; /**< Number of hidden layer nodes */
+    int output_nodes; /**< Number of output nodes */
+    std::vector<std::vector<double>> weights_input_hidden; /**< Weights between input and hidden layers */
+    std::vector<std::vector<double>> weights_hidden_output; /**< Weights between hidden and output layers */
+    std::vector<double> bias_hidden; /**< Biases of hidden layer nodes */
+    std::vector<double> bias_output; /**< Biases of output layer nodes */
+    std::vector<double> hidden_outputs; /**< Outputs of hidden layer neurons */
+    double learning_rate; /**< Learning rate for weight updates */
+    std::vector<int> input; /**< Input data used in backpropagation */
+    std::vector<double> output; /**< Actual output from the network during forward pass */
 
     // Neural network methods
+
+    /**********************************************************************
+     * @brief Reads the states of the buttons
+     * @return Vector containing the states of the buttons
+     **********************************************************************/
     std::vector<int> ReadButtonStates();
+
+    /**********************************************************************
+     * @brief Controls the LED based on the given state
+     * @param state The state to set the LED to
+     **********************************************************************/
     void ControlLED(bool state);
+
+    /**********************************************************************
+     * @brief Trains the neural network using backpropagation
+     * @param inputs Vector of input data
+     * @param targets Vector of target outputs
+     * @param epochs Number of training epochs
+     * @param learning_rate Learning rate for weight updates
+     **********************************************************************/
     void TrainNetwork(const std::vector<std::vector<int>> &inputs, const std::vector<int> &targets, int epochs, double learning_rate);
+
+    /**********************************************************************
+     * @brief Rectified Linear Unit (ReLU) activation function
+     * @param x Input value
+     * @return Output value after applying ReLU
+     **********************************************************************/
     double ReLU(double x);
+
+    /**********************************************************************
+     * @brief Initializes the weights and biases of the neural network
+     **********************************************************************/
     void InitializeWeights();
+
+    /**********************************************************************
+     * @brief Performs forward propagation in the neural network
+     * @param input Vector of input data
+     **********************************************************************/
     void ForwardPropagation(const std::vector<int> &input);
+
+    /**********************************************************************
+     * @brief Performs backpropagation in the neural network
+     * @param target Target output value
+     **********************************************************************/
     void BackPropagation(int target);
+
+    /**********************************************************************
+     * @brief Predicts the output based on the input data
+     * @param input Vector of input data
+     * @return Predicted output value
+     **********************************************************************/
     int Predict(const std::vector<int> &input);
 };
 
